17516c452b64c3b1589b690cb813b72c5adf9a7e
DraftMan <nicovanaarsen@gmail.com>
2018-09-19 20:05:49 +0200

    commande ping et prefix ajoutés

---
+++ commands/bot/ping.js
@@ -0,0 +1,24 @@
+const {Command} = require('discord.js-commando')
+
+module.exports = class PingCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'ping',
+			memberName: 'ping',
+			group: 'utils',
+			aliases: ['connexion','pong'],
+			description: 'Vérifier le ping du bot sur le serveur Discord.',
+			examples: ['ping'],
+		});
+	}
+
+	async run(msg) {
+		if(!msg.editable) {
+			const pingMsg = await msg.reply('Pinging...');
+			return pingMsg.edit(`${msg.channel.type !== 'dm' ? `${msg.author}, ` : ''}Pong! Le retour du message a pris ${pingMsg.createdTimestamp - msg.createdTimestamp}ms.\n${this.client.ping ? `Le ping est de ${Math.round(this.client.ping)}ms.` : ''}`);
+		} else {
+			await msg.edit('Pinging...');
+			return msg.edit(`Pong! Le retour du message a pris ${msg.editedTimestamp - msg.createdTimestamp}ms.\n${this.client.ping ? `Le ping est de ${Math.round(this.client.ping)}ms.` : ''}`);
+		}
+	}
+};
---
+++ commands/utils/prefix.js
@@ -0,0 +1,51 @@
+const {Command} = require('discord.js-commando')
+
+module.exports = class PrefixCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'prefix',
+			memberName: 'prefix',
+			group: 'utils',
+			description: 'Afficher ou changer le prefix.',
+			format: '[prefix/"default"/"none"]',
+			examples: ['prefix', 'prefix ?', 'prefix @', 'prefix default', 'prefix none'],
+			args: [{
+				key: 'prefix',
+				prompt: 'What would you like to set the bot\'s prefix to?',
+				type: 'string',
+				max: 15,
+				default: ''
+			}]
+		});
+	}
+
+	async run(msg, args) {
+		if(!args.prefix) {
+			const prefix = msg.guild ? msg.guild.commandPrefix : this.client.commandPrefix;
+			return msg.reply(`${prefix ? `Le prefix des commandes est \`\`${prefix}\`\`.` : 'Il n\'y a aucun prefix.'}\nA partir de maintenant, veuillez utilisez ${msg.anyUsage('commande')} pour executer une commande.`);
+		}
+
+		if(msg.guild) {
+			if(!msg.member.hasPermission('ADMINISTRATOR') && !this.client.isOwner(msg.author)) {
+				return msg.reply('Uniquement les Administateurs peuvent changer le prefix du bot');
+			}
+		} else if(!this.client.isOwner(msg.author)) {
+			return msg.reply('Seul le propriétaire du bot peut modifier le prefix global.');
+		}
+
+		const lowercase = args.prefix.toLowerCase();
+		const prefix = lowercase === 'none' ? '' : args.prefix;
+		let response;
+		if(lowercase === 'default') {
+			if(msg.guild) msg.guild.commandPrefix = null; else this.client.commandPrefix = null;
+			const current = this.client.commandPrefix ? `\`\`${this.client.commandPrefix}\`\`` : 'aucun prefix';
+			response = `Réinitialisation du prefix commande  (actuellement ${current}).`;
+		} else {
+			if(msg.guild) msg.guild.commandPrefix = prefix; else this.client.commandPrefix = prefix;
+			response = prefix ? `Prefix de commande définit sur \`\`${args.prefix}\`\`.` : 'Suppression du prefix de commande.';
+		}
+
+		await msg.reply(`${response} Pour executer une commande, veuillez utilisez ${msg.anyUsage('commande')}.`);
+		return null;
+	}
+};
