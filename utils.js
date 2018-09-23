1dbc91f02847eca9c28f512feac21f7ebe2bf27f
DraftMan <nicovanaarsen@gmail.com>
2018-09-23 19:07:42 +0200

    Song added to utils

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 const Jimp = require('jimp'),
       path = require('path'),
-      {MessageEmbed,MessageAttachment} = require('discord.js')
+      {MessageEmbed,MessageAttachment,Util} = require('discord.js')
 
 const makeWelcomeImage = async (member) => {
     try {
@@ -42,7 +42,75 @@ const addRole = async (role, member) => {
   }
 }
 
+class Song {
+  constructor (video, member) {
+    this.name = Util.escapeMarkdown(video.title);
+    this.id = video.id;
+    this.length = video.durationSeconds ? video.durationSeconds : video.duration / 1000;
+    this.member = member;
+    this.dispatcher = null;
+    this.playing = false;
+  }
+
+  get url () {
+    return `https://www.youtube.com/watch?v=${this.id}`;
+  }
+
+  get thumbnail () {
+    const thumbnail = `https://img.youtube.com/vi/${this.id}/mqdefault.jpg`;
+
+    return thumbnail;
+  }
+
+  get username () {
+    const name = `${this.member.user.tag} (${this.member.user.id})`;
+
+    return Util.escapeMarkdown(name);
+  }
+
+  get avatar () {
+    const avatar = `${this.member.user.displayAvatarURL({format: 'png'})}`;
+
+    return avatar;
+  }
+
+  get lengthString () {
+    return this.constructor.timeString(this.length);
+  }
+
+  timeLeft (currentTime) {
+    return this.constructor.timeString(this.length - currentTime);
+  }
+
+  toString () {
+    return `${this.name} (${this.lengthString})`;
+  }
+
+  static timeString (seconds, forceHours = false) {
+    const hours = Math.floor(seconds / 3600),
+          minutes = Math.floor(seconds % 3600 / 60);
+
+    return `${forceHours || hours >= 1 ? `${hours}:` : ''}${hours >= 1 ? `0${minutes}`.slice(-2) : minutes}:${`0${Math.floor(seconds % 60)}`.slice(-2)}`;
+  }
+}
+
+const roundNumber = function (num, scale = 0) {
+  if (!String(num).includes('e')) {
+    return Number(`${Math.round(`${num}e+${scale}`)}e-${scale}`);
+  }
+  const arr = `${num}`.split('e');
+  let sig = '';
+
+  if (Number(arr[1]) + scale > 0) {
+    sig = '+';
+  }
+
+  return Number(`${Math.round(`${Number(arr[0])}e${sig}${Number(arr[1]) + scale}`)}e-${scale}`);
+};
+
 module.exports = {
   makeWelcomeImage,
-  addRole
+  addRole,
+  Song,
+  roundNumber
 };
