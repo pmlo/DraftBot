9b318727b89356f5f0ca50181086b7d7602f6493
DraftMan <nicovanaarsen@gmail.com>
2018-10-10 23:12:30 +0200

    commandes queue et status ajoutés

---
+++ commands/musique/queue.js
@@ -0,0 +1,69 @@
+const {Command, util} = require('discord.js-commando'),
+  {oneLine, stripIndents} = require('common-tags'),
+  {Song} = require('../../utils.js');
+
+module.exports = class ViewQueueCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'queue',
+      memberName: 'queue',
+      group: 'musique',
+      aliases: ['songs', 'song-list', 'list', 'listqueue'],
+      description: 'Afficher la liste des musiques en cours',
+      format: '[PageNumber]',
+      examples: ['queue 2'],
+      guildOnly: true,
+      args: [
+        {
+          key: 'page',
+          prompt: 'Quelle page voulez vous voir ?',
+          type: 'integer',
+          default: 1
+        }
+      ]
+    });
+  }
+
+  run (msg, {page}) {
+    const queue = this.queue.get(msg.guild.id);
+
+    if (!queue) {
+      return msg.reply('il n\'y a aucune musique dans la file d\'attente. Pourquoi ne pas y ajouter quelques titres? 😎');
+    }
+
+    const currentSong = queue.songs[0],
+      currentTime = currentSong.dispatcher ? currentSong.dispatcher.streamTime / 1000 : 0,
+      paginated = util.paginate(queue.songs, page, Math.floor(process.env.PAGINATED_ITEMS)),
+      totalLength = queue.songs.reduce((prev, song) => prev + song.length, 0);
+
+    return msg.embed({
+      color: 0xcd6e57,
+      author: {
+        name: `${msg.author.tag} (${msg.author.id})`,
+        iconURL: msg.author.displayAvatarURL({format: 'png'})
+      },
+      description: stripIndents`
+            __**File d'attente, page ${paginated.page}**__
+            ${paginated.items.map(song => `**-** ${!isNaN(song.id) ? `${song.name} (${song.lengthString})` : `[${song.name}](${`https://www.youtube.com/watch?v=${song.id}`})`} (${song.lengthString})`).join('\n')}
+            ${paginated.maxPage > 1 ? `\nVeuillez utiliser ${msg.usage()} pour voir une page en particulier.\n` : ''}
+
+            **En cours:** ${!isNaN(currentSong.id) ? `${currentSong.name}` : `[${currentSong.name}](${`https://www.youtube.com/watch?v=${currentSong.id}`})`}
+            ${oneLine`
+                **Progression:**
+                ${!currentSong.playing ? 'En pause: ' : ''}${Song.timeString(currentTime)} /
+                ${currentSong.lengthString}
+                (${currentSong.timeLeft(currentTime)} restant)
+            `}
+            **Durée totale de la file d'attente:** ${Song.timeString(totalLength)}
+        `
+    });
+  }
+
+  get queue () {
+    if (!this._queue) {
+      this._queue = this.client.registry.resolveCommand('musique:play').queue;
+    }
+
+    return this._queue;
+  }
+};
---
+++ commands/musique/status.js
@@ -0,0 +1,49 @@
+const {Command} = require('discord.js-commando'), 
+  {stripIndents} = require('common-tags'), 
+  {Song} = require('../../utils.js');
+
+module.exports = class MusicStatusCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'status',
+      memberName: 'status',
+      group: 'musique',
+      aliases: ['song', 'playing', 'current-song', 'now-playing'],
+      description: 'Afficher le status de la musique en cours.',
+      guildOnly: true
+    });
+  }
+
+  run (msg) {
+    const queue = this.queue.get(msg.guild.id);
+
+    if (!queue) {
+      return msg.say('Il n\'y a aucune musique dans la file d\'attente. Pourquoi ne pas y ajouter quelques titres? 😎');
+    }
+    const song = queue.songs[0], // eslint-disable-line one-var
+      currentTime = song.dispatcher ? song.dispatcher.streamTime / 1000 : 0,
+      embed = {
+        color: 0xcd6e57,
+        author: {
+          name: `${song.username}`,
+          iconURL: song.avatar
+        },
+        description: stripIndents`
+				[${song}](${`${song.url}`})
+				On est à ${Song.timeString(currentTime)}, il reste ${song.timeLeft(currentTime)} de la musique.
+				${!song.playing ? 'La musique est en pause.' : ''}
+			`,
+        image: {url: song.thumbnail}
+      };
+
+    return msg.embed(embed);
+  }
+
+  get queue () {
+    if (!this._queue) {
+      this._queue = this.client.registry.resolveCommand('musique:play').queue;
+    }
+
+    return this._queue;
+  }
+};
