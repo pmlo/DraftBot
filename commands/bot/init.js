6f91e268ffea4fc7887704da2a2dc47375c38cb8
DraftMan <nicovanaarsen@gmail.com>
2018-10-10 19:02:32 +0200

    New try

--- commands/bot/init.js
+++ commands/bot/init.js
@@ -15,7 +15,6 @@ module.exports = class InviteCommand extends Command {
   }
 
   async run (msg) {
-
     const configEmbed = new MessageEmbed()
       .setTitle('Configuration')
       .setAuthor(msg.author.username,msg.author.displayAvatarURL({format: 'png'}))
@@ -30,77 +29,73 @@ module.exports = class InviteCommand extends Command {
     await this.runProcess(msg, 0);
   }
   lisenCancel(msg) {
-    return (message) => {
+    return async (message) => {
       if(msg.author.id !== message.author.id) return;
   
       if(message.content.toLowerCase() === 'cancel'){
+        await msg.client.removeListener('message', this.lisenCancel(msg));
+        await msg.client.removeListener('messageReactionAdd',this.event);
+        await msg.client.removeListener('message', this.lisenMessages(msg));
         message.reply('configuration annulé !')
-        return msg.client.removeListener('message', this.lisenCancel(msg))
+        return false;
       }
-    }
-  }
-  lisenMessages(msg) {
-    return (message) => {
-      if(msg.author.id !== message.author.id) return;
-      return message;
     }
   }
   async runProcess (msg,process) {
     if(process === 0){
-      const question = await msg.say('Voulez vous un message de bienvenue quand un joueur rejoinds le serveur ? *exemple ci dessous*',{
+      const question = await msg.say({
+        embed: questionEmbed(msg,'Voulez vous un message de bienvenue quand un joueur rejoinds le serveur ? *exemple ci dessous*'),
         file: 'https://www.draftman.fr/images/draftbot/exemple_welcome_message.jpg'
       })
-      await this.trueFalse(question,'Les messages de bienvenue sont maintenant **$1** !');
-      this.runProcess(msg,1)
+      await this.trueFalse(msg,question,'Les messages de bienvenue sont maintenant **$1** !',1);
     }
     if(process === 1){
-      await msg.say('Dans quel salon voulez vous les messages de bienvenue ?');
-      const message = await msg.client.on('message', this.lisenMessages(msg));
-      const channel = await findChannel(message);
-      if(channel !== null) msg.client.removeListener('message', this.lisenMessages(msg));
-      msg.guild.settings.set('welcomeChannel', channel);
+      await msg.embed(questionEmbed(msg,'Dans quel salon voulez vous les messages de bienvenue ?'));
 
-      const embed = new MessageEmbed()
-      .setAuthor(user.username,msg.displayAvatarURL({format: 'png'}))
-      .setColor(0xcd6e57)
-      .setDescription(`Les messages de bienvenue seront maintenant envoyés dans le salon #${channel.name} !`)
-      .setFooter("Procéssus de configuration", msg.client.user.displayAvatarURL({format: 'png'}))
-      .setTimestamp()
-      msg.say(embed)
-      return msg.client.removeListener('message', this.lisenCancel(msg))
+      const lisenChannel = (msg) => {
+        return async (message) => {
+          if(msg.author.id !== message.author.id) return;
+          const channel = await findChannel(message.content, msg);
+          if(channel !== null) msg.client.removeListener('message', lisenChannel(msg));
+          msg.guild.settings.set('welcomeChannel', channel);
+
+          msg.embed(questionEmbed(msg,`Les messages de bienvenue seront maintenant envoyés dans le salon #${channel.name} !`))
+        }
+      }
+      await msg.client.on('message', lisenChannel(msg));
     }
-
-    await msg.client.on('message', this.lisenResponse(msg));
   }
-  async trueFalse(question,response){
+  async trueFalse(msg,question,response,nextProcess){
     const emojis = ['✅','❎']
     await question.react('✅')
     await question.react('❎')
-    question.client.on('messageReactionAdd',this.event);
 
-    const event = () => {
-      return async (messageReaction,user) => {
+    const event = async (messageReaction,user) => {
+        if(user.id === messageReaction.message.client.user.id) return;
         if(messageReaction.message.id === question.id){
-          if(emojis.includes(messageReaction.name)){
+          if(emojis.includes(messageReaction.emoji.name)){
             await question.delete();
             
             const embed = new MessageEmbed()
-            .setAuthor(user.username,msg.displayAvatarURL({format: 'png'}))
+            .setAuthor(user.username,user.displayAvatarURL({format: 'png'}))
             .setColor(0xcd6e57)
-            .setDescription(response.replace('$1',messageReaction.name === '✅' ? 'activés' : 'désactivés'))
+            .setDescription(response.replace('$1',messageReaction.emoji.name === '✅' ? 'activés' : 'désactivés'))
             .setFooter("Procéssus de configuration", msg.client.user.displayAvatarURL({format: 'png'}))
             .setTimestamp()
       
-            await question.say(embed)
+            await msg.say(embed)
             
-            return question.client.removeListener('messageReactionAdd',event);
+            question.client.removeListener('messageReactionAdd',event);
+            
+            return this.runProcess(msg,1,nextProcess)
 
           } else{
-            messageReaction.remove(user)
+            messageReaction.users.remove(user)
           }
         }
-      }
     }
+
+    return question.client.on('messageReactionAdd',event);
   }
 };
 
@@ -108,18 +103,20 @@ const findChannel = (val, msg) => {
   const matches = val.match(/^(?:<#)?([0-9]+)>?$/);
   if(matches) return msg.guild.channels.get(matches[1]) || null;
   const search = val.toLowerCase();
-  const channels = msg.guild.channels.filter(nameFilterInexact(search));
+  const channels = msg.guild.channels.filter(thing => thing.name.toLowerCase().includes(search));
   if(channels.size === 0) return null;
   if(channels.size === 1) return channels.first();
-  const exactChannels = channels.filter(nameFilterExact(search));
+  const exactChannels = channels.filter(filter => filter.name.toLowerCase() === search);
   if(exactChannels.size === 1) return exactChannels.first();
   return null;
 }
 
-const nameFilterExact = (search) => {
-	return thing => thing.name.toLowerCase() === search;
-}
-
-const nameFilterInexact = (search) => {
-	return thing => thing.name.toLowerCase().includes(search);
+const questionEmbed = (msg, question) => {
+  const embed = new MessageEmbed()
+  .setAuthor(msg.author.username,msg.author.displayAvatarURL({format: 'png'}))
+  .setColor(0x39d600)
+  .setDescription(question)
+  .setFooter("Procéssus de configuration", msg.client.user.displayAvatarURL({format: 'png'}))
+  .setTimestamp()
+  return embed;
 }
