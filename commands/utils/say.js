fb0034cb872957f7bcd5539dcc4437aa32dfcf9a
DraftMan <nicovanaarsen@gmail.com>
2018-10-10 19:02:06 +0200

    say command created

---
+++ commands/utils/say.js
@@ -0,0 +1,27 @@
+const {Command} = require('discord.js-commando'), 
+  {MessageEmbed} = require('discord.js')
+
+module.exports = class AvatarCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'say',
+      memberName: 'say',
+      group: 'utils',
+      aliases: ['dit','sayd'],
+      description: 'Permet de faire parler le bot',
+      examples: ['say DraftMan est le plus fort !'],
+      guildOnly: true,
+      args: [
+        {
+          key: 'message',
+          prompt: 'Quel message voulez vous envoyer ?',
+          type: 'message',
+        }
+      ]
+    });
+  }
+
+  run (msg, {message}) {
+    return msg.say(message)
+  }
+};
