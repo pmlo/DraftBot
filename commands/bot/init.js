90d2f1378c07c066b3a7e6d7c623b322f23a59b6
DraftMan <nicovanaarsen@gmail.com>
2018-10-11 22:00:02 +0200

    news tests

--- commands/bot/init.js
+++ commands/bot/init.js
@@ -32,77 +32,25 @@ module.exports = class InviteCommand extends Command {
       .setTimestamp()
 
     msg.embed(configEmbed);
-    msg.client.on('message', this.listenCancel(msg));
+    msg.client.on('message', listenCancel(msg));
     this.runProcess(msg, 0);
   }
-  
-  listenCancel(msg) {
-    return (message) => {
-      if(msg.author.id !== message.author.id) return;
-  
-      if(message.content.toLowerCase() === 'cancel'){
-        msg.client.removeListener('message', this.listenCancel(msg));
-        msg.client.removeListener('messageReactionAdd',this.event);
-        msg.client.removeListener('message', this.listenMessages(msg));
-        message.reply('configuration annulé !')
-        return false;
-      }
-    }
-  }
+
   async runProcess (msg,process) {
     if(process === 0){
       const question = await msg.say({
         embed: questionEmbed(msg,'Voulez vous un message de bienvenue quand un joueur rejoinds le serveur ? *exemple ci dessous*'),
         file: 'https://www.draftman.fr/images/draftbot/exemple_welcome_message.jpg'
       })
-      await this.affirmativeQuestion(msg,question,'Les messages de bienvenue sont maintenant **$1** !',1);
+      return await msg.client.on('messageReactionAdd',affirmativeQuestion(msg,question,'Les messages de bienvenue sont maintenant **$1** !',1));
     }
     if(process === 1){
       await msg.embed(questionEmbed(msg,'Dans quel salon voulez vous les messages de bienvenue ?'));
-
-      const listenChannel = (msg) => {
-        return async (message) => {
-          if(msg.author.id !== message.author.id) return;
-          const channel = await findChannel(message.content, msg);
-          if(channel !== null) msg.client.removeListener('message', listenChannel(msg));
-          msg.guild.settings.set('welcomeChannel', channel);
-
-          msg.embed(questionEmbed(msg,`Les messages de bienvenue seront maintenant envoyés dans le salon #${channel.name} !`))
-        }
-      }
-      await msg.client.on('message', listenChannel(msg));
+      return await msg.client.on('message', listenChannel(msg));
     }
-  }
-  async affirmativeQuestion(msg,question,response,nextProcess){
-    const emojis = ['✅','❎']
-    await Promise.all(emojis.map((emoji) => question.react(emoji)));
-
-    const event = async (messageReaction,user) => {
-        if(user.bot) return;
-        if(messageReaction.message.id === question.id){
-          if(emojis.includes(messageReaction.emoji.name)){
-            await question.delete();
-            
-            const embed = new MessageEmbed()
-            .setAuthor(user.username,user.displayAvatarURL({format: 'png'}))
-            .setColor(0xcd6e57)
-            .setDescription(response.replace('$1',messageReaction.emoji.name === '✅' ? 'activés' : 'désactivés'))
-            .setFooter("Procéssus de configuration", msg.client.user.displayAvatarURL({format: 'png'}))
-            .setTimestamp()
-      
-            await msg.say(embed)
-            
-            question.client.removeListener('messageReactionAdd',event);
-            
-            return this.runProcess(msg,1,nextProcess)
-
-          } else{
-            messageReaction.users.remove(user)
-          }
-        }
-    }
-
-    return question.client.on('messageReactionAdd',event);
+    console.log('no process')
+    msg.embed(questionEmbed(msg,'Félicitation la configuration est terminé, merci !'))
+    return stopCommand(msg)
   }
 };
 
@@ -118,6 +66,52 @@ const findChannel = (val, msg) => {
   return null;
 }
 
+const affirmativeQuestion = async (msg,question,response,nextProcess) => {
+  const emojis = ['✅','❎']
+  await Promise.all(emojis.map((emoji) => question.react(emoji)));
+  return async (messageReaction,user) => {
+    if(user.bot) return;
+    if(messageReaction.message.id === question.id){
+      if(emojis.includes(messageReaction.emoji.name)){
+        await question.delete();
+        
+        const embed = new MessageEmbed()
+        .setAuthor(user.username,user.displayAvatarURL({format: 'png'}))
+        .setColor(0xcd6e57)
+        .setDescription(response.replace('$1',messageReaction.emoji.name === '✅' ? 'activés' : 'désactivés'))
+        .setFooter("Procéssus de configuration", msg.client.user.displayAvatarURL({format: 'png'}))
+        .setTimestamp()
+  
+        await msg.say(embed)
+        
+        question.client.removeListener('messageReactionAdd',affirmativeQuestion(msg,question,response,nextProcess));
+        
+        return this.runProcess(msg,messageReaction.emoji.name === '✅' ? nextProcess : nextProcess+1)
+  
+      } else{
+        messageReaction.users.remove(user)
+      }
+    }
+  }
+}
+
+const listenChannel = (msg) => {
+  return async (message) => {
+    if(msg.author.id !== message.author.id) return;
+    const channel = await findChannel(message.content, msg);
+    if(channel === null) {
+      message.delete({timeout: 2000})
+      return msg.embed(errorEmbed(msg,`Impossible de trouver le salon \`${message}\`, merci de réessayer!`)).then(m => m.delete({timeout: 3000}))
+    }else{
+      msg.client.removeListener('message', listenChannel(msg));
+    }
+    msg.guild.settings.set('welcomeChannel', channel);
+
+    msg.embed(questionEmbed(msg,`Les messages de bienvenue seront maintenant envoyés dans le salon #${channel.name} !`))
+    return this.runProcess(msg,2)
+  }
+}
+
 const questionEmbed = (msg, question) => {
   return new MessageEmbed()
   .setAuthor(msg.author.username,msg.author.displayAvatarURL({format: 'png'}))
@@ -125,4 +119,31 @@ const questionEmbed = (msg, question) => {
   .setDescription(question)
   .setFooter("Procéssus de configuration", msg.client.user.displayAvatarURL({format: 'png'}))
   .setTimestamp();
+}
+
+const errorEmbed = (msg, message) => {
+  return new MessageEmbed()
+  .setAuthor(msg.author.username,msg.author.displayAvatarURL({format: 'png'}))
+  .setColor(0xce0000)
+  .setDescription(message)
+  .setFooter("Procéssus de configuration", msg.client.user.displayAvatarURL({format: 'png'}))
+  .setTimestamp();
+}
+
+const stopCommand = (msg) => {
+  msg.client.removeListener('message', listenCancel(msg));
+  msg.client.removeListener('messageReactionAdd',affirmativeQuestion(msg));
+  msg.client.removeListener('message', listenChannel(msg));
+  return this.run(msg)
+}
+
+const listenCancel = (msg) => {
+  return (message) => {
+    if(msg.author.id !== message.author.id) return;
+
+    if(message.content.toLowerCase() === 'cancel'){
+      message.reply('configuration annulé !')
+      return stopCommand(msg);
+    }
+  }
 }
