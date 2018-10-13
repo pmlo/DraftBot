d8ecd4cf48cc1ca4f61410b26badb9377d168bc3
DraftMan <nicovanaarsen@gmail.com>
2018-10-13 14:40:29 +0200

    new try with a system clean

--- commands/bot/init.js
+++ commands/bot/init.js
@@ -1,6 +1,7 @@
 const { MessageEmbed } = require('discord.js')
 const { Command } = require('discord.js-commando')
-const {oneLine, stripIndents} = require('common-tags')
+const { stripIndents } = require('common-tags')
+const { findChannel } = require('../../utils.js')
 
 module.exports = class InviteCommand extends Command {
   constructor (client) {
@@ -38,80 +39,126 @@ module.exports = class InviteCommand extends Command {
 
   async runProcess (msg,process) {
     const current = this;
-    if(process === 0){
-      const question = await msg.say({
-        embed: questionEmbed(msg,'Voulez vous un message de bienvenue quand un joueur rejoinds le serveur ? *exemple ci dessous*'),
-        file: 'https://www.draftman.fr/images/draftbot/exemple_welcome_message.jpg'
-      })
-      return msg.client.on('messageReactionAdd', await affirmativeQuestion(current)(msg,question,'Les messages de bienvenue sont maintenant **$1** !',1));
-    }
     if(process === 1){
-      await msg.embed(questionEmbed(msg,'Dans quel salon voulez vous les messages de bienvenue ?'));
-      return msg.client.on('message', listenChannel(current)(msg));
+      return welcomeMessage(current)(msg,process)
     }
-    console.log('no process')
+    if(process === 2){
+      return channelWelcome(current)(msg,process)
+    }
+
+    // Il n'y pas plus de process
     msg.embed(questionEmbed(msg,'Félicitation la configuration est terminé, merci !'))
     return stopCommand(current)(msg)
   }
 };
 
-const findChannel = (val, msg) => {
-  const matches = val.match(/^(?:<#)?([0-9]+)>?$/);
-  if(matches) return msg.guild.channels.get(matches[1]) || null;
-  const search = val.toLowerCase();
-  const channels = msg.guild.channels.filter(thing => thing.name.toLowerCase().includes(search));
-  if(channels.size === 0) return null;
-  if(channels.size === 1) return channels.first();
-  const exactChannels = channels.filter(filter => filter.name.toLowerCase() === search);
-  if(exactChannels.size === 1) return exactChannels.first();
-  return null;
-}
+/* 
+* Etapes de la commande 
+*
+* Différents étapes de pour avoir les réponses de l'utilisateur
+*/
 
-const affirmativeQuestion = (current) => async (msg,question,response,nextProcess) => {
+const welcomeMessage = (current) => async (msg,process) => {
+  const nextProcess = process + 1;
   const emojis = ['✅','❎']
-  await Promise.all(emojis.map((emoji) => question.react(emoji)));
-  return async (messageReaction,user) => {
-    if(user.bot) return;
-    if(messageReaction.message.id === question.id){
-      if(emojis.includes(messageReaction.emoji.name)){
-        
-        const embed = new MessageEmbed()
-        .setAuthor(user.username,user.displayAvatarURL({format: 'png'}))
-        .setColor(0xcd6e57)
-        .setDescription(response.replace('$1',messageReaction.emoji.name === '✅' ? 'activés' : 'désactivés'))
-        .setFooter("Procéssus de configuration", msg.client.user.displayAvatarURL({format: 'png'}))
-        .setTimestamp()
 
-        await msg.say(embed)
-        
-        question.client.removeListener('messageReactionAdd',await affirmativeQuestion(current)(msg,question,response,nextProcess));
-        
-        await messageReaction.message.delete();
+  const question = await msg.say({
+    embed: questionEmbed(msg,'Voulez vous un message de bienvenue quand un joueur rejoinds le serveur ? *exemple ci dessous*'),
+    file: 'https://www.draftman.fr/images/draftbot/exemple_welcome_message.jpg'
+  })
+  
+  await Promise.all(emojis.map(emoji => question.react(emoji)));
 
-        current.runProcess(msg,messageReaction.emoji.name === '✅' ? nextProcess : nextProcess+1)
-      } else{
-        messageReaction.users.remove(user)
-      }
-    }
-  }
+
+  const response = eventListenReactions(msg,question,emojis).resultat;
+
+  msg.guild.settings.set('welcomeMessage', response);
+
+
+  await msg.embed(resultEmbed(msg,`Les messages de bienvenue sont maintenant **${response === true ? 'activés' : 'désactivés'}** !`))
+  current.runProcess(msg, response === true ? nextProcess : nextProcess+1)
 }
 
-const listenChannel = (current) => (msg) => {
+
+const channelWelcome = (current) => async (msg,process) => {
+  const nextProcess = process + 1;
+  await msg.embed(questionEmbed(msg,'Dans quel salon voulez vous les messages de bienvenue ?'));
+
+
+  const channel = eventListenChannel(msg).resultat;
+
+
+  msg.guild.settings.set('welcomeChannel', channel);
+
+  msg.embed(resultEmbed(msg,`Les messages de bienvenue seront maintenant envoyés dans le salon #${channel.name} !`))
+  current.runProcess(msg,nextProcess)
+}
+
+/* 
+* Events des étapes
+*
+* Différents events pour capturer les réponses de l'utilisateur
+*/
+
+const eventListenChannel = (current) => (msg) => {
   return async (message) => {
     if(msg.author.id !== message.author.id) return;
     const channel = await findChannel(message.content, msg);
     if(channel === null) {
       message.delete({timeout: 2000})
-      return msg.embed(errorEmbed(msg,`Impossible de trouver le salon \`${message}\`, merci de réessayer!`)).then(m => m.delete({timeout: 3000}))
+      msg.embed(errorEmbed(msg,`Impossible de trouver le salon \`${message}\`, merci de réessayer!`)).then(m => m.delete({timeout: 3000}))
+      return;
     }else{
-      msg.client.removeListener('message', listenChannel(current)(msg));
+      msg.client.removeListener('message', eventListenChannel(current)(msg));
     }
-    msg.guild.settings.set('welcomeChannel', channel);
-
-    msg.embed(questionEmbed(msg,`Les messages de bienvenue seront maintenant envoyés dans le salon #${channel.name} !`))
-    return current.runProcess(msg,2)
+    return channel
   }
 }
+
+const eventListenReactions = (current) => (msg,question,reactions) => {
+  return async (messageReaction,user) => {
+    if(user.bot) return;
+    if(messageReaction.message.id !== question.id) return;
+    if(reactions.includes(messageReaction.emoji.name)){
+      messageReaction.users.remove(user)
+      return;
+    }
+    msg.client.removeListener('message', eventListenReactions(current)(msg,reactions));
+    messageReaction.message.delete();
+    return messageReaction.emoji.name === '✅' ? true : false;
+  }
+}
+
+/* 
+* Events Cancel & Stop
+*
+* Différents events pour mener à bien le procéssus de la commande
+*/
+
+const eventCancel = (msg) => {
+  return (message) => {
+    if(msg.author.id !== message.author.id) return;
+
+    if(message.content.toLowerCase() === 'cancel'){
+      message.reply('configuration annulé !')
+      return stopCommand(msg);
+    }
+  }
+}
+
+const stopCommand = (current) => async (msg) => {
+  msg.client.removeListener('message', listenCancel(msg));
+  msg.client.removeListener('message', listenChannel(current)(msg));
+  msg.client.removeListener('messageReactionAdd',await affirmativeQuestion(current)(msg,msg,'',0));
+  return current.run(msg);
+}
+
+
+/* 
+* Embeds 
+*
+* Embeds pour créer les réponses plus facilement
+*/
 
 const questionEmbed = (msg, question) => {
   return new MessageEmbed()
@@ -131,20 +178,11 @@ const errorEmbed = (msg, message) => {
   .setTimestamp();
 }
 
-const stopCommand = (current) => (msg) => {
-  msg.client.removeListener('message', listenCancel(msg));
-  msg.client.removeListener('messageReactionAdd',affirmativeQuestion(current)(msg));
-  msg.client.removeListener('message', listenChannel(current)(msg));
-  return this.run(msg)
-}
-
-const listenCancel = (msg) => {
-  return (message) => {
-    if(msg.author.id !== message.author.id) return;
-
-    if(message.content.toLowerCase() === 'cancel'){
-      message.reply('configuration annulé !')
-      return stopCommand(msg);
-    }
-  }
+const resultEmbed = (msg, conclusion) => {
+  return new MessageEmbed()
+  .setAuthor(msg.author.username,msg.author.displayAvatarURL({format: 'png'}))
+  .setColor(0xcd6e57)
+  .setDescription(conclusion)
+  .setFooter("Procéssus de configuration", msg.client.user.displayAvatarURL({format: 'png'}))
+  .setTimestamp();
 }
