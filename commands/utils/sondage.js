a74121147e1bf8023e1af321b884b08d17a0e170
DraftMan <nicovanaarsen@gmail.com>
2018-10-14 20:10:44 +0200

    🆕 Nouvelle commande | Création de la commande de sondage

---
+++ commands/utils/sondage.js
@@ -0,0 +1,38 @@
+const {Command} = require('discord.js-commando'),
+      {MessageEmbed} = require('discord.js')
+
+module.exports = class QuoteCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'sondage',
+      memberName: 'sondage',
+      group: 'utils',
+      aliases: ['vote'],
+      description: 'Lancer un sondage pour la communauté',
+      examples: ['sondage Aimez vous les frites ?'],
+      guildOnly: true,
+      args: [
+        {
+          key: 'message',
+          prompt: 'Quel est l\'affirmation de votre sondage',
+          type: 'string'
+        }
+      ]
+    });
+  }
+
+  async run (msg, {message}) {
+
+    const emojis = ['✅','❌']
+
+    const embed = new Discord.RichEmbed()
+    .setDescription(":newspaper: Sondage")
+    .addField(message,'Veuillez voter avec :white_check_mark: et :x:')
+    .setColor(0xcd6e57)
+    .setTimestamp()
+
+    await msg.embed(embed)
+    await msg.delete();
+    await Promise.all(emojis.map(emoji => question.react(emoji)));
+  }
+}
