1433f63ea90ad85e79c3087000b855b900269984
DraftMan <nicovanaarsen@gmail.com>
2018-10-29 23:30:51 +0100

    getGuildId command added

---
+++ commands/utils/getGuildId.js
@@ -0,0 +1,27 @@
+const {Command} = require('discord.js-commando'), 
+  {MessageEmbed} = require('discord.js')
+
+module.exports = class AvatarCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'guildid',
+      memberName: 'guildid',
+      group: 'utils',
+      aliases: ['getguildid'],
+      description: 'Récupérer l\'id d\'une guild',
+      examples: ['guildid'],
+      guildOnly: true
+    });
+  }
+
+  run (msg) {
+    const embed = new MessageEmbed()
+      .setColor(0xcd6e57)
+      .setAuthor(msg.author.tag, msg.author.displayAvatarURL())
+      .setDescription(`L'id de la guild est: \`\`\`${msg.guild.id}\`\`\``)
+      .setFooter(msg.guild.name,msg.guild.iconURL({format: 'png'}))
+      .setTimestamp()
+
+    return msg.embed(embed);
+  }
+};
