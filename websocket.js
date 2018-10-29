b140aa25f2b0024ee80cfae0866a0d4211bf0898
DraftMan <nicovanaarsen@gmail.com>
2018-10-29 23:28:48 +0100

    websocket added

---
+++ websocket.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+class WebSocket {
+
+    constructor(token, port, client) {
+        this.token = token
+        this.port = port
+        this.client = client
+        this.app = express()
+
+        this.app.get('/api/commands', (req, res) => res.json({ commands: this.client.registry.groups.map(grp => grp.commands) }))
+
+        this.server = this.app.listen(port, () => {
+            console.log("Websocket API set up at port " + this.server.address().port)
+        })
+    }
+}
+
+module.exports = WebSocket
