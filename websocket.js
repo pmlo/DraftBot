f71bd0331074d6af8a377833bde131d4963ae0aa
DraftMan <nicovanaarsen@gmail.com>
2018-11-06 23:09:10 +0100

    loadash added

--- websocket.js
+++ websocket.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const lodash = require('lodash');
 const https = require ('https'); 
 const fs = require ('fs'); 
 
@@ -12,7 +13,7 @@ class WebSocket {
 
         this.app.use(express.static('static'));
 
-        this.app.get('/api/commands', (req, res) => res.status(200).send({ commands: this.client.registry.groups.map(grp => grp.commands) }))
+        this.app.get('/api/commands', (req, res) => res.status(200).send({ commands: lodash.flatten(this.client.registry.groups.map(grp => grp.commands))}))
 
         this.server = this.app.listen(port, () => {
             console.log("Websocket API set up at port " + this.server.address().port)
