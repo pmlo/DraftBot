03c1d3992cc7b5e447d8f5305faa0ce36bb50af7
DraftProducts <nicovanaarsen@gmail.com>
2018-11-25 11:11:01 +0100

    commande maths crée

---
+++ commands/utils/maths.js
@@ -0,0 +1,42 @@
+const {Command} = require('discord.js-commando')
+const {MessageEmbed} = require('discord.js')
+const fetch = require('node-fetch')
+
+module.exports = class AvatarCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'maths',
+      memberName: 'maths',
+      group: 'utils',
+      aliases: ['calc','calculatrice','equation','math'],
+      description: 'Résoudre une équation',
+      examples: ['math PI * 3'],
+      args: [
+        {
+          key: 'equation',
+          prompt: 'Quelle équation souaitez vous résoudre ?',
+          type: 'string',
+          parse: p => p.toLowerCase().replace(/x/gim, '*'),
+        }
+      ]
+    });
+  }
+
+  run (msg, {equation}) {
+
+    const calculator = await fetch('http://api.mathjs.org/v4/', {
+      body: JSON.stringify({ expr: equation }),
+      method: 'POST',
+    });
+    const maths = await calculator.json();
+
+    const mathEmbed = new MessageEmbed()
+    .setTitle('Calculatrice')
+    .setColor(0xcd6e57)
+    .setDescription(oneLine`La réponse à \`${equation.toString()}\` est \`${maths.result}\``)
+    .setFooter(msg.guild ? msg.guild.name : '',msg.guild ? msg.guild.iconURL({format: 'png'}) : msg.guild.client.user.avatarURL({format: 'png'}))
+    .setTimestamp()
+
+    return msg.embed(mathEmbed);
+  }
+};
