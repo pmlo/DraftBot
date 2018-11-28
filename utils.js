a7ade6f96a5da1d900556529fff6a9c5795d2095
DraftProducts <nicovanaarsen@gmail.com>
2018-11-28 23:24:06 +0100

    test

--- utils.js
+++ utils.js
@@ -412,7 +412,7 @@ const addUserXp = (msg,user,newXp) => new Promise((resolve, reject) =>{
 const removeUserXp = (msg,user,newXp) => new Promise((resolve, reject) =>{
   return sqlite.open(path.join(__dirname, './storage.sqlite'))
   .then(connexion => connexion.get(`SELECT xp FROM "levels" WHERE user= ${user.id} AND guild= ${msg.guild.id}`).then(xp => ({connexion, xp})))
-  .then(({connexion, xp}) => console.log(xp - newXp) /*connexion.post(`UPDATE user FROM "levels" WHERE user= ${user.id} AND guild= ${msg.guild.id} SET xp= ${xp - newXp}`)*/)
+  .then(({connexion, xp}) => console.log(xp.xp - newXp) /*connexion.post(`UPDATE user FROM "levels" WHERE user= ${user.id} AND guild= ${msg.guild.id} SET xp= ${xp - newXp}`)*/)
   .then(resolve(true))
   .catch(() => reject(false))
 })
