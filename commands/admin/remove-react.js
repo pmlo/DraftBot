475ce9ee1b636dd4162b838838dfd02a0ed06406
DraftProducts <nicovanaarsen@gmail.com>
2018-12-31 13:04:05 +0100

    sqlite removed

--- commands/admin/remove-react.js
+++ commands/admin/remove-react.js
@@ -1,6 +1,5 @@
 const {Command} = require('discord.js-commando');
 const {MessageEmbed,Util} = require('discord.js');
-const sqlite = require('sqlite');
 const path = require('path');
 const Database = require('better-sqlite3');
 
