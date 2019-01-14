fb962f3d8209f9da59d46a1d60b0fcbe49fec4fe
DraftMan <nicovanaarsen@gmail.com>
2019-01-14 09:01:01 +0100

    création des commandes enable et disable

---
+++ commands/admin/disable.js
@@ -0,0 +1,46 @@
+const {Command} = require('discord.js-commando');
+const {MessageEmbed} = require('discord.js');
+const {deleteCommandMessages} = require('../../utils.js');
+
+module.exports = class DisableCommandCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'disable',
+			aliases: ['disable-command', 'cmd-off', 'command-off','désactiver','desactiver','desactiver-commande'],
+			group: 'admin',
+			memberName: 'disable',
+			description: 'Désactiver une commande ou un groupe de commandes.',
+			examples: ['disable levels', 'disable prefix'],
+			guarded: true,
+			args: [
+				{
+					key: 'cmdOrGrp',
+					label: 'command/group',
+					prompt: 'Quelle commande ou quel groupe souhaitez vous désactiver ?',
+					type: 'group|command'
+				}
+			]
+		});
+	}
+
+	hasPermission(msg) {
+		if(!msg.guild) return this.client.isOwner(msg.author);
+		return msg.member.hasPermission('ADMINISTRATOR') || this.client.isOwner(msg.author);
+	}
+
+	run(msg, args) {
+		deleteCommandMessages(msg)
+		if(!args.cmdOrGrp.isEnabledIn(msg.guild, true)) {
+			return msg.reply(
+				`${args.cmdOrGrp.group ? 'La commande' : 'Le groupe'} \`${args.cmdOrGrp.name}\` est déjà désactivé.`
+			);
+		}
+		if(args.cmdOrGrp.guarded) {
+			return msg.reply(
+				`Vous ne pouvez pas désactiver ${args.cmdOrGrp.group ? 'la commande' : 'le groupe'} \`${args.cmdOrGrp.name}\`.`
+			);
+		}
+		args.cmdOrGrp.setEnabledIn(msg.guild, false);
+		return msg.reply(`${args.cmdOrGrp.group ? 'La commande' : 'Le groupe'} \`${args.cmdOrGrp.name}\`est maintenant désactivé.`);
+	}
+};
---
+++ commands/admin/enable.js
@@ -0,0 +1,52 @@
+const {Command} = require('discord.js-commando');
+const {MessageEmbed} = require('discord.js');
+const {deleteCommandMessages} = require('../../utils.js');
+
+module.exports = class EnableCommandCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'enable',
+			aliases: ['enable-command', 'cmd-on', 'command-on','activer','activer-commande'],
+			group: 'admin',
+			memberName: 'enable',
+			description: 'Activer une commande ou un groupe de commandes.',
+			examples: ['enable levels', 'enable prefix'],
+			guarded: true,
+			args: [
+				{
+					key: 'cmdOrGrp',
+					label: 'command/group',
+					prompt: 'Quelle commande ou quel groupe souhaitez vous activer ?',
+					type: 'group|command'
+				}
+			]
+		});
+	}
+
+	hasPermission(msg) {
+		if(!msg.guild) return this.client.isOwner(msg.author);
+		return msg.member.hasPermission('ADMINISTRATOR') || this.client.isOwner(msg.author);
+	}
+
+	run(msg, args) {
+		deleteCommandMessages(msg)
+		const group = args.cmdOrGrp.group;
+		if(args.cmdOrGrp.isEnabledIn(msg.guild, true)) {
+			return msg.reply(
+				`${args.cmdOrGrp.group ? 'La commande' : 'Le groupe'} \`${args.cmdOrGrp.name}\`  est déjà activé ${
+					group && !group.isEnabledIn(msg.guild) ?
+					`, mais le groupe \`${group.name}\` est désactivé, donc il ne peut toujours pas être utilisé` :
+					''
+				}.`
+			);
+		}
+		args.cmdOrGrp.setEnabledIn(msg.guild, true);
+		return msg.reply(
+			`${group ? 'La commande' : 'Le groupe'} \`${args.cmdOrGrp.name}\` ${
+				group && !group.isEnabledIn(msg.guild) ?
+				`, mais le groupe \`${group.name}\` est désactivé, donc il ne peut toujours pas être utilisé` :
+				''
+			}.`
+		);
+	}
+};
