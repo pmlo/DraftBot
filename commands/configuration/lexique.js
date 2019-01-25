507e6fe491f583609a6f61f39c32f9a8ad33ce7b
DraftMan <nicovanaarsen@gmail.com>
2019-01-25 20:36:57 +0100

    Lexique réglé !

--- commands/configuration/lexique.js
+++ commands/configuration/lexique.js
@@ -40,14 +40,14 @@ module.exports = class LexiqueCommand extends Command {
     if(argument) {
       if(msg.guild.settings.get('badwords')){
         const badwords = msg.guild.settings.get('badwords');
-        const oldWords = badwords.mots.split(',')
+        const oldWords = badwords.mots !== '' ? badwords.mots.split(',') : []
 
         if(['add','ajouter'].includes(argument)){
           if(oldWords.includes(mot)) return msg.reply('Ce mot est déjà interdit !')
           
           oldWords.push(mot);
 
-          msg.guild.settings.set('badwords', {mots: oldWords.join(','), status: badwords.status});
+          msg.guild.settings.set('badwords', {mots: oldWords.length > 1 ? oldWords.join(',') : oldWords[0], status: badwords.status});
           description = `🎉 La liste des mots interdit est maintenant: \`${oldWords.join('\`, \`')}\` !`;
   
           if(badwords.status !== true){
@@ -58,8 +58,8 @@ module.exports = class LexiqueCommand extends Command {
         if(['enlever','retirer','remove','supprimer'].includes(argument)){
           if(!oldWords.includes(mot)) return msg.reply('Ce mot n\'est pas dans la liste des mots interdits !')
           const newWords = oldWords.filter(w => w !== mot);
-          
-          msg.guild.settings.set('badwords', {mots: newWords.join(','), status: badwords.status});
+
+          msg.guild.settings.set('badwords', {mots: newWords.length > 0 ? newWords.join(',') : '', status: badwords.status});
           description = `🎉 La liste des mots interdit est maintenant: \`${newWords.join('\`, \`')}\` !`;
   
           if(badwords.status !== true){
@@ -67,20 +67,34 @@ module.exports = class LexiqueCommand extends Command {
           }
         }
         if(['list','show','liste'].includes(argument)){   
-
-          description = `🎉 La liste des mots interdit est: \`${oldWords.join('\`, \`')}\` !`;
+          if(oldWords.length === 0) {
+            description = `La liste des mots interdit est vide, vous pouvez en ajouter en faisant \`${msg.guild.commandPrefix}lexique add <votre mot>\` !`; 
+          }else{
+            description = `🎉 La liste des mots interdit est: \`${oldWords.join('\`, \`')}\` !`;
+          }
         }
       }else{
-        msg.guild.settings.set('badwords', {mots: mot, status: true});
+        if(['add','ajouter'].includes(argument)){
+          msg.guild.settings.set('badwords', {mots: mot, status: true});
+          description = `🎉 La liste des mots interdit est maintenant: \`${oldWords.join('\`, \`')}\` !`;
+        }
+        if(['enlever','retirer','remove','supprimer'].includes(argument)){
+          msg.guild.settings.set('badwords', {mots: '', status: true});
+          return msg.reply('Ce mot n\'est pas dans la liste des mots interdits !')
+        }
+        if(['list','show','liste'].includes(argument)){
+          msg.guild.settings.set('badwords', {mots: '', status: true});
+          description = `🎉 La liste des mots interdit est vide, vous pouvez en ajouter en faisant \`${msg.guild.commandPrefix}lexique add <votre mot>\` !`;
+        }
         description = `🎉 Le lexique des mots interdit viens d'être **activé**, vous y avez ajouté les mots suivants: \`${mots.join('\`, \`')}\` !`;
       }
     }else{
       if(msg.guild.settings.get('badwords') && msg.guild.settings.get('badwords').status == true) {
         msg.guild.settings.set('badwords', {mots: msg.guild.settings.get('badwords') ? msg.guild.settings.get('badwords').mots : '', status: false});
-        description = `🎉 Les messages de bienvenue sont maintenant **désactivés** !`;
+        description = `🎉 La restriction du vocablulaire est maintenant **désactivé** !`;
       }else{
         msg.guild.settings.set('badwords', {mots: msg.guild.settings.get('badwords') ? msg.guild.settings.get('badwords').mots : '', status: true});
-        description = `🎉 Les messages de bienvenue sont maintenant **activés** !`;
+        description = `🎉 La restriction du vocablulaire est maintenant **activé** !`;
       }
     }
 
