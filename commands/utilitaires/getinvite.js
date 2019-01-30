59e98b7ae77adf1561c272ab5dd2f1bac71b0c16
DraftMan <nicovanaarsen@gmail.com>
2019-01-30 08:37:20 +0100

    getinvite | changement de process

--- commands/utilitaires/getinvite.js
+++ commands/utilitaires/getinvite.js
@@ -6,7 +6,7 @@ module.exports = class InviteCommandCommand extends Command {
 	constructor(client) {
 		super(client, {
 			name: 'getinvite',
-			group: 'utils',
+			group: 'utilitaires',
 			memberName: 'getinvite',
 			description: 'Récupérer l\'invitation d\'un serveur à partir de son nom.',
 			ownerOnly: true,
@@ -24,14 +24,13 @@ module.exports = class InviteCommandCommand extends Command {
 
 		if(!newGuild) return msg.reply('impossible de trouver une guild ayant ce nom !')
 		
-		newGuild.fetchInvites()
-		.then(invites => {
-			if(!invites) return msg.reply('il n\'y a aucune invitation valide sur ce serveur !')
+		newGuild.systemChannel.createInvite()
+		.then(invite => {
 			const embed = new MessageEmbed()
 			.setTitle(newGuild.name)
 			.setThumbnail(newGuild.iconURL({format: 'png'}))
 			.setColor(0xcd6e57)
-			.setDescription(`Voici une invitation pour rejoindre **${newGuild.name}**\n${invites.map(invite => invite.url).join('\n')}`)
+			.setDescription(`Voici une invitation pour rejoindre **${newGuild.name}**\n${invite.url}`)
 			.setTimestamp()
 
 			msg.embed(embed)
